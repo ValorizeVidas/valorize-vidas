e2a101efc2241b6bad057d28a6bca792fdc608a0
Mikael <mikael.costa@ilegra.com>
2019-04-15 14:24:48 -0300

    - Insert google analytics;

---
+++ src/config/analytics.js
@@ -0,0 +1,9 @@
+export default function() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+
+  gtag("config", "UA-138375806-1");
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import router from './router'
-import App from './App.vue'
-import './assets/sass/_styles.scss'
+import router from "./router";
+import App from "./App.vue";
+import "./assets/sass/_styles.scss";
+import "./config/analytics";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
