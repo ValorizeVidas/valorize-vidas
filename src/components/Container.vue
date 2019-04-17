2efa2993d844352aff7a85ff444f98ff6ba6cf8d
Lucas Inocente <olucassanchez@gmail.com>
2019-04-17 13:48:52 -0300

    Hotfix/manutencao (#97)
    * Remove menções ao CVV na documentação
    
    * Remove informações equivodadas da home
    
    * Remove menu de acesso à informações

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -11,59 +11,8 @@
       <div class="column is-full-tablet is-4-desktop">
         <div class="mensagem">
           <p>
-            Pode ficar tranquilo que podemos te ajudar. Entre em contato com a gente da forma como
-            preferir
+            Estamos em manutenção. A Valorize Vidas não tem nenhum vínculo com o CVV.
           </p>
-        </div>
-      </div>
-    </div>
-    <div class="columns is-multiline cards">
-      <div class="column is-full-tablet is-half-desktop cards-container-column">
-        <div class="columns is-variable is-4 is-centered-tablet-only">
-          <div class="column is-half-desktop">
-            <span class="cards-title">Precisa de ajuda?</span>
-          </div>
-        </div>
-        <div class="columns is-variable is-4">
-          <div class="column card-column">
-            <a @click="openChat()">
-              <Card :title="'Chat'" :icon="'comments-regular'" :bgColor="'#F38C18'" />
-            </a>
-          </div>
-          <div class="column card-column">
-            <Card :title="'Ligue 188'" :icon="'mobile-alt-solid'" :bgColor="'#F38C18'" />
-          </div>
-        </div>
-        <div class="columns is-variable is-4">
-          <div class="column card-column">
-            <router-link to="/contato">
-              <Card :title="'Email'" :bgColor="'#F38C18'" />
-            </router-link>
-          </div>
-          <div class="column card-column">
-            <router-link to="/enderecos">
-              <Card :title="'Endereços'" :bgColor="'#F38C18'" />
-            </router-link>
-          </div>
-        </div>
-      </div>
-      <div class="column is-full-tablet is-half-desktop cards-container-column">
-        <div class="columns is-variable is-4 is-centered-tablet-only">
-          <div class="column is-half-desktop">
-            <span class="cards-title">Quer ajudar?</span>
-          </div>
-        </div>
-        <div class="columns is-variable is-4">
-          <div class="column card-column">
-            <a href="http://apoia.se/valorizevidas" target="_blank">
-              <Card :title="'Contribuir'" :icon="'hand-holding-usd-solid'" :bgColor="'#E8AE28'" />
-            </a>
-          </div>
-          <div class="column card-column">
-            <router-link to="/voluntariar">
-              <Card :title="'Voluntariar'" :icon="'hand-holding-heart-solid'" :bgColor="'#F7C248'" />
-            </router-link>
-          </div>
         </div>
       </div>
     </div>
